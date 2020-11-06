0af336298f726750f57cdf8e6cca450c8ab4e9d1
Seung Lee <41555063+jkobb510@users.noreply.github.com>
2020-11-06 18:23:20 -0500

    Update waves.php

--- Group Project 2/waves.php
+++ Group Project 2/waves.php
@@ -11,23 +11,23 @@
 
 <body>
    <?php session_start(); ?>
-   <form action="answer.php" method="POST" class="container">
+   <form action="answersD.php" method="POST" class="container">
 
       <div class="title"> Category: Science </div>
        
 
       <div id="question" class="question"> Electromagnetic radiation includes  </div>
 
-      <label class="option"><input type="radio" name="question-9-answers" value="A" />radio waves <span
+      <label class="option"><input type="radio" name="question-answers" value="A" />radio waves <span
             id="question-answers-A">Microwaves</span></label>
 
-      <label class="option"><input type="radio" name="question-9-answers" value="B" /> infrared waves<span
+      <label class="option"><input type="radio" name="question-answers" value="B" /> infrared waves<span
             id="question-answers-B">Radio waves</span></label>
 
-      <label class="option"><input type="radio" name="question-9-answers" value="C" />microwaves <span
+      <label class="option"><input type="radio" name="question-answers" value="C" />microwaves <span
             id="question-answers-C">UV Light</span></label>
 
-      <label class="option"><input type="radio" name="question-9-answers" value="D" />All of the above <span
+      <label class="option"><input type="radio" name="question-answers" value="D" />All of the above <span
             id="question-answers-D">All of the above</span></label>
 
       <input class="button" type="submit" value="Submit">
