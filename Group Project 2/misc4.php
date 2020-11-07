152205e0d9026fc3ae9f0dc212dfa8bfcfba1c1a
Seung Lee <41555063+jkobb510@users.noreply.github.com>
2020-11-06 19:01:23 -0500

    Update misc4.php

--- Group Project 2/misc4.php
+++ Group Project 2/misc4.php
@@ -11,23 +11,23 @@
 
 <body>
    <?php session_start(); ?>
-   <form action="answer.php" method="POST" class="container">
+   <form action="answersB.php" method="POST" class="container">
 
       <div class="title"> Category: Miscellaneous </div>
        
 
       <div id="question" class="question">Which of the following is NOT a child of Kanye West? </div>
 
-      <label class="option"><input type="radio" name="question-9-answers" value="A" /><span
+      <label class="option"><input type="radio" name="question-answers" value="A" /><span
             id="question-answers-A">Saint West</span></label>
 
-      <label class="option"><input type="radio" name="question-9-answers" value="B" /> <span
+      <label class="option"><input type="radio" name="question-answers" value="B" /> <span
             id="question-answers-B">South West</span></label>
 
-      <label class="option"><input type="radio" name="question-9-answers" value="C" /> <span
+      <label class="option"><input type="radio" name="question-answers" value="C" /> <span
             id="question-answers-C">North West</span></label>
 
-      <label class="option"><input type="radio" name="question-9-answers" value="D" /> <span
+      <label class="option"><input type="radio" name="question-answers" value="D" /> <span
             id="question-answers-D">Chicago West</span></label>
 
       <input class="button" type="submit" value="Submit">
