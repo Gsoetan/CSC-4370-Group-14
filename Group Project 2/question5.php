9a7d1d74cf15c455302d56dd1043028695ef4389
Seung Lee <41555063+jkobb510@users.noreply.github.com>
2020-11-07 14:47:26 -0500

    Update question5.php

--- Group Project 2/question5.php
+++ Group Project 2/question5.php
@@ -13,11 +13,11 @@
    <form action="answersD.php" method="POST" class="container">
     <div class="title"> Category: "Technology" </div>
 
-      <div id="question" class="question"> Widely considered as the father of Artificial Intelligence, this man developed a test for determining whether or not a computer is capable of thinking like a human being 
+      <div id="question" class="question"> The founder of SpaceX is
   </div>
 
       <label class="option"><input type="radio" name="question-answers" value="A" /> <span
-            id="question-4-answers-A">John von Neumann</span></label>
+            id="question-4-answers-A">Mark Zuckerberg</span></label>
 
       <label class="option"><input type="radio" name="question-answers" value="B" /> <span
             id="question-4-answers-B">Elon Musk</span></label>
@@ -26,7 +26,7 @@
             id="question-4-answers-C">Dennis Ritchie</span></label>
 
       <label class="option"><input type="radio" name="question-answers" value="D" /> <span
-            id="question-4-answers-D">Alan Turing</span></label>
+            id="question-4-answers-D">Jeff Bezos</span></label>
 
       <input class="button" type="submit" value="Submit">
       
