42849566f7e53f790cec7113e536fd61ab495a7e
Seung Lee <41555063+jkobb510@users.noreply.github.com>
2020-11-07 13:41:19 -0500

    Update medicine.php

--- Group Project 2/medicine.php
+++ Group Project 2/medicine.php
@@ -1,4 +1,7 @@
-
+<?php 
+session_save_path("/home/~username/public_html/groupproject2/sessions");
+session_start(); 
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -17,16 +20,16 @@
 
       <div id="question" class="question">The discovery of this drug changed the course medicine as it was the first effective drug to fight off infection. </div>
 
-      <label class="option"><input type="radio" name="question-answers" value="A" />radio waves <span
+      <label class="option"><input type="radio" name="question-answers" value="A" /><span
             id="question-answers-A"></span>Aspirin drug</label>
 
-      <label class="option"><input type="radio" name="question-answers" value="B" /> infrared waves<span
+      <label class="option"><input type="radio" name="question-answers" value="B" /> <span
             id="question-answers-B">Tylenol</span></label>
 
-      <label class="option"><input type="radio" name="question-answers" value="C" />microwaves <span
+      <label class="option"><input type="radio" name="question-answers" value="C" />  <span
             id="question-answers-C">Penicillin</span></label>
 
-      <label class="option"><input type="radio" name="question-answers" value="D" />All of the above <span
+      <label class="option"><input type="radio" name="question-answers" value="D" /><span
             id="question-answers-D">Advil</span></label>
 
       <input class="button" type="submit" value="Submit">
