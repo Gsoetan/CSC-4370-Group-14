633ccf7aefff1d0284138b385885129a17602e05
Seung Lee <41555063+jkobb510@users.noreply.github.com>
2020-11-07 14:08:59 -0500

    Update misc1.php

--- Group Project 2/misc1.php
+++ Group Project 2/misc1.php
@@ -15,18 +15,18 @@
       <div class="title"> Category: Miscellaneous </div>
        
 
-      <div id="question" class="question"> n the 1970s, thos underground urban movement formed in Bronx, New York City </div>
+      <div id="question" class="question"> n the 1970s, this underground urban movement formed in Bronx, New York City </div>
 
-      <label class="option"><input type="radio" name="question-answers" value="A" />radio waves <span
+      <label class="option"><input type="radio" name="question-answers" value="A" /><span
             id="question-answers-A">House</span></label>
 
-      <label class="option"><input type="radio" name="question-answers" value="B" /> infrared waves<span
+      <label class="option"><input type="radio" name="question-answers" value="B" /><span
             id="question-answers-B">Disco</span></label>
 
-      <label class="option"><input type="radio" name="question-answers" value="C" />microwaves <span
+      <label class="option"><input type="radio" name="question-answers" value="C" /><span
             id="question-answers-C">Funk</span></label>
 
-      <label class="option"><input type="radio" name="question-answers" value="D" />All of the above <span
+      <label class="option"><input type="radio" name="question-answers" value="D" /><span
             id="question-answers-D">Hip-Hop</span></label>
 
       <input class="button" type="submit" value="Submit">
