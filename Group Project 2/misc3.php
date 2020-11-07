2c42bfe02089f8fd59fb76ab35082e8799f7c198
Seung Lee <41555063+jkobb510@users.noreply.github.com>
2020-11-06 19:00:30 -0500

    Update misc3.php

--- Group Project 2/misc3.php
+++ Group Project 2/misc3.php
@@ -18,16 +18,16 @@
 
       <div id="question" class="question">The youngest U.S. cabinet department is the Department of  </div>
 
-      <label class="option"><input type="radio" name="question-9-answers" value="A" />radio waves <span
+      <label class="option"><input type="radio" name="question-answers" value="A" />radio waves <span
             id="question-answers-A">Homeland Security</span></label>
 
-      <label class="option"><input type="radio" name="question-9-answers" value="B" /> infrared waves<span
+      <label class="option"><input type="radio" name="question-answers" value="B" /> infrared waves<span
             id="question-answers-B">Health and Human Services</span></label>
 
-      <label class="option"><input type="radio" name="question-9-answers" value="C" />microwaves <span
+      <label class="option"><input type="radio" name="question-answers" value="C" />microwaves <span
             id="question-answers-C">Education</span></label>
 
-      <label class="option"><input type="radio" name="question-9-answers" value="D" />All of the above <span
+      <label class="option"><input type="radio" name="question-answers" value="D" />All of the above <span
             id="question-answers-D">Energy</span></label>
 
       <input class="button" type="submit" value="Submit">
